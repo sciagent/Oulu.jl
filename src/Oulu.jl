383219a519c23076603e55d398f2b9b9fd9bf09b
Carsten Bauer <crstnbr@gmail.com>
2020-02-12 11:54:51 +0200

    awesome functionality

--- src/Oulu.jl
+++ src/Oulu.jl
@@ -2,4 +2,8 @@ module Oulu
 
 greet() = print("Hello World!")
 
+function myfunc(x)
+    return x+2
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using Oulu
 using Test
 
 @testset "Oulu.jl" begin
-    # Write your own tests here.
+    @test Oulu.myfunc(5) == 7
+    @test Oulu.myfunc(1.2) == 3.2
 end
