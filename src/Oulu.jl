7e6afcd582756a131b0529b382da7ca2d2182bf6
sciagent <sciagent@users.noreply.github.com>
2020-03-09 16:55:00 +0200

    Update Oulu.jl

--- src/Oulu.jl
+++ src/Oulu.jl
@@ -6,4 +6,12 @@ function myfunc(x)
     return x+2
 end
 
+function coronavirusupdate() //by sciagent
+    r = HTTP.get("https://www.worldometers.info/coronavirus/", cookies=true);
+    data = String(r.body)
+    println(data[findfirst(r"Coronavirus Update*?(.*) Wuhan China Virus Outbreak"s, data)])
+end
+
+coronavirusupdate()
+
 end # module
